30930a105a93694043a3fd69f94c0a029ab41f22
kharismarizqii <kharisma.bakhittah@traveloka.com>
2023-03-12 15:13:23 +0700

    dynamic start and end line position

--- draggable-element.js
+++ draggable-element.js
@@ -33,9 +33,11 @@ function dragElement(elmnt) {
         pos3 = e.clientX;
         pos4 = e.clientY;
         // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        updateLine(elmnt.offsetTop - pos2, elmnt.offsetLeft - pos1)
+        let top = elmnt.offsetTop - pos2
+        let left = elmnt.offsetLeft - pos1
+        elmnt.style.top = top + "px";
+        elmnt.style.left = left + "px";
+        updateLine()
     }
 
     function closeDragElement() {
@@ -44,18 +46,37 @@ function dragElement(elmnt) {
         document.onmousemove = null;
     }
 
-    function updateLine(top, left) {
+    function updateLine() {
         let lineId = document.querySelector(`line[id*=${elmnt.id}]`).id
         let isStartingLine = lineId.slice(0, elmnt.id.length) == elmnt.id
-        let x = left + "px"
-        let y = top + (elmnt.offsetHeight / 2) + "px"
+        let secondBoxId = (isStartingLine) ? lineId.substring(elmnt.id.length + 4, lineId.length) : lineId.substring(0, lineId.indexOf("-to-"))
+        let secondBox = document.getElementById(secondBoxId)
+        let secondBoxOffsetRight = secondBox.offsetLeft + secondBox.offsetWidth
+        let secondBoxHorizontalOffsetCenter = secondBox.offsetLeft + (secondBox.offsetWidth / 2)
+        let currentBoxHorizontalOffsetCenter = elmnt.offsetLeft + (secondBox.offsetWidth/ 2 )
+        let currentBoxOffsetRight = elmnt.offsetLeft + secondBox.offsetWidth
+        let y = elmnt.offsetTop + (elmnt.offsetHeight / 2) + "px"
         let line = document.getElementById(lineId)
+        let circ1 = document.getElementById(`circ-1-of-${line.id}`)
+        let circ2 = document.getElementById(`circ-2-of-${line.id}`)
         if (isStartingLine) {
-            line.setAttribute('x1', x)
+            let x2 = (currentBoxHorizontalOffsetCenter > secondBoxHorizontalOffsetCenter) ? secondBoxOffsetRight: secondBox.offsetLeft + "px"
+            let x1 = (currentBoxHorizontalOffsetCenter > secondBoxHorizontalOffsetCenter) ? elmnt.offsetLeft : currentBoxOffsetRight + "px"
+            line.setAttribute('x1', x1)
             line.setAttribute('y1', y)
+            line.setAttribute('x2', x2)
+            circ1.setAttribute('cx', x1)
+            circ1.setAttribute('cy', y)
+            circ2.setAttribute('cx', x2)
         } else {
-            line.setAttribute('x2', x)
+            let x1 = (currentBoxHorizontalOffsetCenter > secondBoxHorizontalOffsetCenter) ? secondBoxOffsetRight : secondBox.offsetLeft + "px"
+            let x2 = (currentBoxHorizontalOffsetCenter > secondBoxHorizontalOffsetCenter) ? elmnt.offsetLeft : currentBoxOffsetRight + "px"
+            line.setAttribute('x2', x2)
             line.setAttribute('y2', y)
+            line.setAttribute('x1', x1)
+            circ2.setAttribute('cx', x2)
+            circ2.setAttribute('cy', y)
+            circ1.setAttribute('cx', x1)
         }
     }
 }
@@ -65,11 +86,15 @@ function renderLines() {
     var secondBox = document.getElementById("mydiv2")
 
     let line = document.getElementById(`${firstBox.id}-to-${secondBox.id}`)
-
-    console.log(firstBox.style.top)
+    let circ1 = document.getElementById(`circ-1-of-${line.id}`)
+    let circ2 = document.getElementById(`circ-2-of-${line.id}`)
 
     line.setAttribute('x1', "0px")
     line.setAttribute('y1', firstBox.offsetHeight / 2 + "px")
     line.setAttribute('x2', "0px")
     line.setAttribute('y2', secondBox.offsetHeight / 2 + "px")
+    circ1.setAttribute('cx', 0)
+    circ1.setAttribute('cy', firstBox.offsetHeight / 2)
+    circ2.setAttribute('cx', 0)
+    circ2.setAttribute('cy', firstBox.offsetHeight / 2)
 }
